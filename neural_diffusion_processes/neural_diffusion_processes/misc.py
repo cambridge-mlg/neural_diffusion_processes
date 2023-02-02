67dc16d4453d07ea30d175edcb23940955552a74
Vincent Dutordoir <dutordoirv@gmail.com>
2023-02-02 18:00:20 +0000

    add notebook

--- neural_diffusion_processes/neural_diffusion_processes/misc.py
+++ neural_diffusion_processes/neural_diffusion_processes/misc.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import jax
 import jax.numpy as jnp
 
@@ -10,9 +12,10 @@ from .constants import JITTER
 @check_shapes(
     "mean: [num_points, 1]",
     "cov: [num_points, num_points]",
-    "return: [num_samples, num_points, 1]"
+    "return: [num_samples, num_points, 1] if num_samples",
+    "return: [num_points, 1] if not num_samples"
 )
-def sample_mvn(key, mean: Array, cov: Array, num_samples: int = 10):
+def sample_mvn(key, mean: Array, cov: Array, num_samples: Optional[int] = 10):
     """Returns samples from a GP(mean, kernel) at x."""
     L = jnp.linalg.cholesky(cov + JITTER * jnp.eye(len(mean)))
     eps = jax.random.normal(key, (len(mean), num_samples))
