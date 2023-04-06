cf9c0b482ccbfef5a717c546581eb10f23e67570
Emile Mathieu <emile.mathieu@stats.ox.ac.uk>
2023-04-06 12:14:54 +0100

    update 2d notebook

--- notebooks/gaussian-process-interpolation-nd.py
+++ notebooks/gaussian-process-interpolation-nd.py
@@ -45,7 +45,7 @@ key = jax.random.PRNGKey(42)
 key, subkey = jax.random.split(key)
 
 output_dim = 2
-beta_schedule = ndp.sde.LinearBetaSchedule(t0 = 1e-4, beta0 = 1e-4, beta1 = 15.0)
+beta_schedule = ndp.sde.LinearBetaSchedule(t0 = 1e-3, beta0 = 1e-4, beta1 = 4.0)
 x = radial_grid_2d(10, 30)
 
 # k0 = ndp.kernels.RBFVec(output_dim)
@@ -57,10 +57,10 @@ k0_params = {"variance": k0_variance, "lengthscale": 2.23606797749979}
 k0 = SumKernel([k0, ndp.kernels.WhiteVec(output_dim)])
 k0_params = [k0_params, {"variance": 0.02}]
 
-# k1 = ndp.kernels.WhiteVec(output_dim)
 k1 = ndp.kernels.RBFVec(output_dim)
+# k1 = ndp.kernels.WhiteVec(output_dim)
+# k1_params = {"variance": k0_variance, "lengthscale": 2.}
 k1_params = {"variance": k0_variance, "lengthscale": 2.}
-
 k1 = SumKernel([k1, ndp.kernels.WhiteVec(output_dim)])
 k1_params = [k1_params, {"variance": 1.}]
 
@@ -71,15 +71,15 @@ limiting_params = {
         "mean_function": mean_function.init_params(key),
     }
 
-kxx = k0.gram(k0_params, x).to_dense()
-kxx = rearrange(kxx, '(n1 p1) (n2 p2) -> n1 n2 p1 p2', p1=output_dim, p2=output_dim) 
-kxx = rearrange(kxx, 'n1 n2 p1 p2 -> (p1 n1) (p2 n2)') 
-plt.matshow(kxx)
+# kxx = k0.gram(k0_params, x).to_dense()
+# kxx = rearrange(kxx, '(n1 p1) (n2 p2) -> n1 n2 p1 p2', p1=output_dim, p2=output_dim) 
+# kxx = rearrange(kxx, 'n1 n2 p1 p2 -> (p1 n1) (p2 n2)') 
+# plt.matshow(kxx)
 
-kxx = k1.gram(limiting_params["kernel"], x).to_dense()
-kxx = rearrange(kxx, '(n1 p1) (n2 p2) -> n1 n2 p1 p2', p1=output_dim, p2=output_dim) 
-kxx = rearrange(kxx, 'n1 n2 p1 p2 -> (p1 n1) (p2 n2)') 
-plt.matshow(kxx)
+# kxx = k1.gram(limiting_params["kernel"], x).to_dense()
+# kxx = rearrange(kxx, '(n1 p1) (n2 p2) -> n1 n2 p1 p2', p1=output_dim, p2=output_dim) 
+# kxx = rearrange(kxx, 'n1 n2 p1 p2 -> (p1 n1) (p2 n2)') 
+# plt.matshow(kxx)
 
 sde = ndp.sde.SDE(k1, mean_function, limiting_params, beta_schedule, True, True, exact=True)
 
@@ -90,39 +90,6 @@ plot_cov = partial(plot_covariances, scale=0.3/math.sqrt(k0_variance), zorder=-1
 from jaxlinop import identity
 solve_lower_triangular = partial(jax.scipy.linalg.solve_triangular, lower=True)  # L⁻¹ x
 solve_upper_triangular = partial(jax.scipy.linalg.solve_triangular, lower=False)  # U⁻¹ x
-
-
-# def score(sde, t: Array, yt: Array, x: Array, kernel) -> Array:
-#     def pt(t):
-#         return sde.pt(t, y0=jnp.zeros_like(yt), k0=k0, k0_params=k0_params)
-    
-#     # mu_t, k_t, params = pt(t)
-#     b = yt# - mu_t({}, x)
-#     n = b.shape[0] * b.shape[1]
-#     b = flatten(b)
-    
-#     # Sigma_t = k_t.gram(params['kernel'], x)
-#     # Sigma_t = Sigma_t._add_diagonal(identity(n) * JITTER)
-#     # Sigma_inv_b = Sigma_t.solve(b)
-
-#     SigmaT = sde.limiting_kernel.gram(sde.limiting_params["kernel"], x)
-#     cov_coef = jnp.exp(-sde.beta_schedule.B(t))
-#     Sigma_t = (1 - cov_coef) * SigmaT.to_dense()
-#     Sigma_t += cov_coef * k0.gram(k0_params, x).to_dense()
-#     Sigma_t += JITTER * jnp.eye(n)
-#     Lt = jnp.linalg.cholesky(Sigma_t)
-#     Sigma_inv_b = solve_upper_triangular(jnp.transpose(Lt), solve_lower_triangular(Lt, b))
-
-#     if kernel:
-#         out = - (SigmaT @ Sigma_inv_b)
-#     else:
-#         out = - Sigma_inv_b
-
-#     return unflatten(out, yt.shape[-1])
-
-# sde.score = lambda key, t, yt, x, network, kernel=True: score(sde, t, yt, x, kernel)
-# score(sde, 0.5, ys[0, -1], x)
-
 
 def get_timesteps(t0, t1, num_ts):
     ts = jnp.exp(jnp.linspace(t0, t1, num_ts))
@@ -193,7 +160,7 @@ key, *subkeys = jax.random.split(key, 1+num_samples)
 rev_out2 = jax.vmap(reverse_solve)(np.stack(subkeys), yT)
 
 plot(rev_out, ts)
-plot(rev_out2, ts)
+# plot(rev_out2, ts)
 
 #%%
 # Solve for conditional samples
@@ -202,7 +169,7 @@ key = jax.random.PRNGKey(0)
 
 x_test = radial_grid_2d(10, 30)
 idx = jax.random.permutation(key, jnp.arange(len(x_test)))
-x_known = x_test[idx[:20]] + 1e-2
+x_known = x_test[idx[:20]] + 1e-5
 
 from neural_diffusion_processes.kernels import prior_gp, posterior_gp
 num_samples = 20
@@ -216,14 +183,198 @@ data_posterior_gp = posterior_gp(mean_function, k0, {"kernel": k0_params, "mean_
 y_test = unflatten(data_posterior_gp.sample(seed=subkey, sample_shape=(num_samples)), output_dim)
 # y_test = unflatten(data_posterior_gp.mean(), output_dim)[None, ...]
 
-num_steps = 100
-num_inner_steps = 10
+def conditional_sample2(
+    sde: SDE,
+    network,
+    x_context,
+    y_context,
+    x_test,
+    *,
+    key,
+    num_steps: int = 100,
+    num_inner_steps: int = 5,
+    prob_flow: bool = True,
+    langevin_kernel = True,
+    psi: float = 1.,
+    lambda0: float = 1.,
+    tau: float = None,
+):
+    # TODO: Langevin dynamics option
+
+    num_context = len(x_context)
+    num_target = len(x_test)
+    y_dim = y_context.shape[-1]
+    shape_augmented_state = [(num_context + num_target) * y_dim]
+    x_augmented = jnp.concatenate([x_context, x_test], axis=0)
+
+    t0 = sde.beta_schedule.t0
+    t1 = sde.beta_schedule.t1
+    ts = jnp.linspace(t1, t0, num_steps, endpoint=True)
+    dt = ts[0] - ts[1]
+    tau = tau if tau is not None else t1
+
+    solver = dfx.Euler()
+
+    diffusion = lambda t, yt, arg: sde.diffusion(t, unflatten(yt, y_dim), arg)
+    if not prob_flow:
+        # reverse SDE:
+        reverse_drift_sde = lambda t, yt, arg: flatten(sde.reverse_drift_sde(key, t, unflatten(yt, y_dim), arg, network))
+
+        shape = jax.ShapeDtypeStruct(shape_augmented_state, y_context.dtype)
+        key, subkey = jax.random.split(key)
+        bm = dfx.VirtualBrownianTree(t0=t1, t1=t0, tol=dt, shape=shape, key=key)
+        terms_reverse = dfx.MultiTerm(
+            dfx.ODETerm(reverse_drift_sde), LinOpControlTerm(diffusion, bm)
+        )
+    else:
+        # reverse ODE:
+        reverse_drift_ode = lambda t, yt, arg: flatten(
+            sde.reverse_drift_ode(key, t, unflatten(yt, y_dim), arg, network)
+            # reverse_drift_ode(sde, key, t, unflatten(yt, y_dim), arg, network)
+        )
+        terms_reverse = dfx.ODETerm(reverse_drift_ode)
+
+    # langevin dynamics:
+    def reverse_drift_langevin(t, yt, x) -> Array:
+        yt = unflatten(yt, y_dim)
+        score = flatten(sde.score(key, t, yt, x, network))
+        if langevin_kernel:
+            if sde.precond:
+                score = score
+            else:
+                score = sde.limiting_gram(x) @ score
+        else:
+            if sde.precond:
+                score = sde.limiting_gram(x).solve(score)
+            else:
+                score = score
+        return 0.5 * sde.beta_schedule(t) * score
+        # return 0.5 * score
+    
+    def diffusion_langevin(t, yt, x):
+        if langevin_kernel:
+            return diffusion(t, yt, x)
+            # return sde.limiting_gram(x)._add_diagonal(JITTER * identity(x.shape[0]*x.shape[1])).to_root()
+        else:
+            return jnp.sqrt(sde.beta_schedule(t)) * identity(yt.shape[-1])
+            # return identity(yt.shape[-1])
+
+    key, subkey = jax.random.split(key)
+    shape = jax.ShapeDtypeStruct(shape_augmented_state, y_context.dtype)
+    # bm = dfx.VirtualBrownianTree(t0=t0, t1=t1, tol=dt, shape=shape, key=subkey)
+    bm = dfx.UnsafeBrownianPath(shape=shape, key=subkey)
+    langevin_terms = dfx.MultiTerm(
+        dfx.ODETerm(reverse_drift_langevin),
+        LinOpControlTerm(diffusion_langevin, bm)
+    )
+
+    def sample_marginal(key, t, x_context, y_context):
+        if len(y_context) == 0:
+            return y_context
+        else:
+            return flatten(sde.sample_marginal(key, t, x_context, y_context))
+
+
+    def inner_loop(key, ys, t):
+        print("compiling Langevin inner_loop")
+        # reverse step
+        yt, yt_context = ys
+        yt_context = sample_marginal(key, t, x_context, y_context) # NOTE: should resample?
+        yt_augmented = jnp.concatenate([yt_context, yt], axis=0)
+
+        # yt_m_dt, *_ = solver.step(
+        #     langevin_terms,
+        #     t - dt,
+        #     t,
+        #     # t + dt,
+        #     yt_augmented,
+        #     x_augmented,
+        #     None,
+        #     made_jump=False,
+        # )
+
+        yt_m_dt = yt_augmented
+        yt_m_dt += lambda0 * psi * dt * reverse_drift_langevin(t - dt, yt_augmented, x_augmented)
+        # noise = jnp.sqrt(psi) * langevin_terms.contr(t - dt, t)[1]
+        noise = jnp.sqrt(psi) * jnp.sqrt(dt) * jax.random.normal(key, shape=yt_augmented.shape)
+        yt_m_dt += diffusion_langevin(t - dt, yt_augmented, x_augmented) @ noise
+        # yt_m_dt += langevin_terms.contr(t, t)[0] * langevin_terms.vf(t, yt_augmented, x_augmented)[0]
+        # yt_m_dt += langevin_terms.vf(t, yt_augmented, x_augmented)[1] @ noise
+        
+        yt = yt_m_dt[num_context * y_dim :]
+        # strip context from augmented state
+        return (yt, yt_context), yt_m_dt
+
+    def outer_loop(key, yt, t):
+        print("compiling Euler-Maruyama outer_loop")
+        # yt_context = sde.sample_marginal(key, t, x_context, y_context)
+        yt_context = sample_marginal(key, t, x_context, y_context)
+        # yt_context = y_context #NOTE: doesn't need to be noised?
+        yt_augmented = jnp.concatenate([yt_context, yt], axis=0)
+
+        yt_m_dt, *_ = solver.step(
+            terms_reverse,
+            t,
+            t - dt,
+            yt_augmented,
+            x_augmented,
+            None,
+            made_jump=False,
+        )
+        # yt_m_dt = yt_augmented
+        # yt_m_dt += -dt * reverse_drift_sde(t, yt_augmented, x_augmented)
+        # # yt_m_dt += terms_reverse.contr(t, t-dt) * terms_reverse.vf(t, yt_augmented, x_augmented)
+        # noise = jax.random.normal(key, shape=yt_augmented.shape)
+        # yt_m_dt += jnp.sqrt(dt) * diffusion(t, yt_augmented, x_augmented) @ noise
+
+        yt = yt_m_dt[num_context * y_dim :]
+        # if num_inner_steps > 0:
+        def corrector(key, yt, yt_context, t):
+            print("corrector")
+            _, yt_m_dt = jax.lax.scan(
+                lambda ys, key: inner_loop(key, ys, t),
+                (yt, yt_context),
+                jax.random.split(key, num_inner_steps),
+            )
+            yt = yt_m_dt[-1][num_context * y_dim :]
+            return yt
+    
+        yt = jax.lax.cond(
+            tau > t,
+            corrector,
+            lambda key, yt, yt_context, t: yt,
+            key, yt, yt_context, t
+        )
+        return yt, yt
+
+    key, subkey = jax.random.split(key)
+    yT = flatten(sde.sample_prior(subkey, x_test))
+
+    xs = (ts[:-1], jax.random.split(key, len(ts) - 1))
+    y0, _ = jax.lax.scan(lambda yt, x: outer_loop(x[1], yt, x[0]), yT, xs)
+    return unflatten(y0, y_dim)
+
+
+
+num_steps = 50
+num_inner_steps = 50
 print(f"num_steps={num_steps} and num_inner_steps={num_inner_steps}")
-conditional_sample = jax.jit(lambda  x, y, x_eval, key: ndp.sde.conditional_sample2(sde, None, x, y, x_eval, key=key, num_steps=num_steps, num_inner_steps=num_inner_steps, langevin_kernel=True, alpha=1.))
+import time
+start = time.time()
+# conditional_sample = jax.jit(lambda  x, y, x_eval, key: ndp.sde.conditional_sample2(sde, None, x, y, x_eval, key=key, num_steps=num_steps, num_inner_steps=num_inner_steps, langevin_kernel=True, alpha=3.))
+conditional_sample = jax.jit(lambda  x, y, x_eval, key: conditional_sample2(sde, None, x, y, x_eval, key=key, num_steps=num_steps, num_inner_steps=num_inner_steps, langevin_kernel=True, tau=.5, psi=2., lambda0=1., prob_flow=True))
 
-samples = jax.vmap(lambda key:conditional_sample(x_known, y_known, x_test, key=key))(jax.random.split(key, num_samples))
+samples = jax.jit(jax.vmap(lambda key:conditional_sample(x_known, y_known, x_test, key=key)))(jax.random.split(key, num_samples))
+end = time.time()
+print(f"time={end - start:.2f}")
+mse_mean_pred = jnp.sum((samples.mean(0) - y_test.mean(0)) ** 2, -1).mean(0)
+print(f"mean mse={mse_mean_pred:.2f}")
+sample_cov = jax.vmap(partial(jax.numpy.cov, rowvar=False), in_axes=[1])(samples)
+true_cov = jax.vmap(partial(jax.numpy.cov, rowvar=False), in_axes=[1])(y_test)
+mse_cov_pred = jnp.sum((sample_cov - true_cov).reshape(true_cov.shape[0], -1) ** 2, -1).mean(0)
+print(f"cov mse={mse_cov_pred:.2f}")
 
-# %%
+
 # Plotting conditional vs true posterior gP
 fig, axes = plt.subplots(2, 2, sharex=True, sharey=True, figsize=(8*2, 8*2))
 fig.subplots_adjust(wspace=0, hspace=0.)
