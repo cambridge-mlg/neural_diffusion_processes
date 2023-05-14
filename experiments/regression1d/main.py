e8c0f1ac1ebbd05f4530aa9033b16f6685fb8c38
Vincent Dutordoir <dutordoirv@gmail.com>
2023-05-14 19:19:51 +0100

    Run May 14

--- experiments/regression1d/main.py
+++ experiments/regression1d/main.py
@@ -78,7 +78,7 @@ except:
     from config import Config, toy_config
 
 
-EXPERIMENT = "regression1d-May14"
+EXPERIMENT = "regression1d-May14-2"
 
 
 _DATETIME = datetime.datetime.now().strftime("%b%d_%H%M%S")
