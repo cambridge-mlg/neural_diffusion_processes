fadf2135820ca96177f34adfcad62fa5cca05ca3
Emile Mathieu <emile.mathieu@stats.ox.ac.uk>
2023-05-22 15:28:37 +0100

    predictive likelihood

--- experiments/regression1d/test_log_prob.py
+++ experiments/regression1d/test_log_prob.py
@@ -1,5 +1,4 @@
-
-#%%
+# %%
 from typing import Mapping
 from dataclasses import dataclass
 
@@ -13,7 +12,6 @@ import neural_diffusion_processes as ndp
 from neural_diffusion_processes.data import regression1d
 
 from neural_diffusion_processes import config
-
 
 
 @dataclass
@@ -43,12 +41,18 @@ key = jax.random.PRNGKey(0)
 beta = ndp.sde.LinearBetaSchedule(t0=5e-4)
 meanT = gpjax.mean_functions.Zero()
 kernelT = ndp.kernels.get_kernel("white", active_dims=[0])
+# kernelT = ndp.kernels.get_kernel("RBF", active_dims=[0])
+# kernelT = ndp.kernels.SumKernel(
+# [kernelT, ndp.kernels.get_kernel("white", active_dims=[0])]
+# )
 paramsT = {
     "mean_function": {},
     "kernel": kernelT.init_params(None),
-    "noise_variance": 0.0
+    # "noise_variance": 1.0,
+    "noise_variance": 0.0,
 }
-paramsT["kernel"]["variance"] = 1.
+# paramsT["kernel"]["variance"] = 1.0
+# paramsT["kernel"][-1]["variance"] = 0.05
 p_ref = GP(meanT, kernelT, paramsT)
 
 sde = ndp.sde.SDE(
@@ -56,13 +60,14 @@ sde = ndp.sde.SDE(
     meanT,
     paramsT,
     beta,
-    is_score_preconditioned=False,
+    # is_score_preconditioned=False,
+    is_score_preconditioned=True,
     std_trick=False,
     residual_trick=False,
 )
 
 
-factory = regression1d._DATASET_FACTORIES[DATASET] 
+factory = regression1d._DATASET_FACTORIES[DATASET]
 assert isinstance(factory, regression1d.GPFunctionalDistribution)
 mean0, kernel0, params0 = factory.mean, factory.kernel, factory.params
 print(params0)
@@ -73,18 +78,31 @@ key, bkey = jax.random.split(key)
 
 
 def log_prob(x, y, key):
-    return ndp.sde.log_prob(sde, true_score_network, x, y, key=key, rtol=1e-6, atol=1e-6)[0][0]
+    return ndp.sde.log_prob(
+        sde, true_score_network, x, y, key=key, rtol=1e-8, atol=1e-8
+    )[0][0]
 
 
 def log_prob_cond(x, y, xc, yc, key):
-    return ndp.sde.log_prob(sde, true_score_network, x, y, x_known=xc, y_known=yc, key=key, rtol=1e-6, atol=1e-6)[0][0]
+    return ndp.sde.log_prob(
+        sde,
+        true_score_network,
+        x,
+        y,
+        x_known=xc,
+        y_known=yc,
+        key=key,
+        rtol=1e-8,
+        atol=1e-8,
+    )[0][0]
     # return ndp.sde.log_prob(sde, true_score_network, x, y, x_known=xc, y_known=yc, key=key, rtol=1e-6, atol=1e-6, hutchinson_type="Gaussian")[0][0]
 
 
 values = []
 
 
-for ji in [1e-6, 1e-8, 1e-12]:
+# for ji in [1e-6, 1e-8, 1e-12]:
+for ji in [1e-8]:
     config.set_config(config.Config(jitter=ji))
     batch = regression1d.get_batch(bkey, 4, DATASET, "training")
     x = jnp.concatenate([batch.xc, batch.xs], axis=1)[0]
@@ -94,20 +112,22 @@ for ji in [1e-6, 1e-8, 1e-12]:
     logp_joint = log_prob(x, y, jkey)
     logp_context = log_prob(batch.xc[0], batch.yc[0], ckey)
     logp_ndp_cond = logp_joint - logp_context
-    logp_ndp_cond2 = log_prob_cond(x, y, batch.xc[0]+1e-8, batch.yc[0], jkey)
+    logp_ndp_cond2 = log_prob_cond(x, y, batch.xc[0] + 1e-8, batch.yc[0], jkey)
 
     logp_gp_prior = p_data.log_prob(x, y)
     D = (batch.xc[0], batch.yc[0])
     logp_gp_cond = p_data.log_prob(batch.xs[0], batch.ys[0], data=D)
 
-    values.append({
-        "jitter": ji,
-        "logp_ndp_prior": logp_joint,
-        "logp_gp_prior": logp_gp_prior,
-        "logp_ndp_cond (diff)": logp_ndp_cond,
-        "logp_ndp_cond (direct)": logp_ndp_cond2,
-        "logp_gp_cond": logp_gp_cond,
-    })
+    values.append(
+        {
+            "jitter": ji,
+            "logp_ndp_prior": logp_joint,
+            "logp_gp_prior": logp_gp_prior,
+            "logp_ndp_cond (diff)": logp_ndp_cond,
+            "logp_ndp_cond (direct)": logp_ndp_cond2,
+            "logp_gp_cond": logp_gp_cond,
+        }
+    )
     print(values[-1])
 
 import pandas as pd
