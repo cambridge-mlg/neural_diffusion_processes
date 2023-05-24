922258fd3e5d2bf010c830326732b2fbea2a8c40
Emile Mathieu <emile.mathieu@stats.ox.ac.uk>
2023-05-24 16:41:45 +0200

    minor

--- tests/test_transformer.py
+++ tests/test_transformer.py
@@ -1,28 +1,14 @@
-from typing import Callable, Optional
-from functools import partial
+import jax
 import pytest
 
-import jax
-
 jax.config.update("jax_enable_x64", True)
-from jax import vmap
-import jax.random as jr
-import jax.numpy as jnp
 import haiku as hk
-import numpy as np
-import e3nn_jax as e3nn
-from einops import rearrange
+import jax.numpy as jnp
+from hydra import compose, initialize
+from hydra.utils import call, instantiate
 
-from hydra import initialize, compose
-from hydra.utils import instantiate, call
-
-from neural_diffusion_processes.models.transformer import TransformerModule
 from neural_diffusion_processes.utils.tests import (
-    _check_permutation_invariance,
-    _check_permutation_equivariance,
-    _check_e2_equivariance,
-)
-import neural_diffusion_processes as ndp
+    _check_e2_equivariance, _check_permutation_equivariance)
 
 with initialize(config_path="../experiments/steerable_gp/config", version_base="1.3.2"):
     overrides = [
@@ -48,21 +34,6 @@ with initialize(config_path="../experiments/steerable_gp/config", version_base="
     @pytest.fixture(name="inputs")
     def _inputs_fixuture(rng):
         rng, next_rng = jax.random.split(rng)
-        # data = call(
-        #     cfg.data,
-        #     key=next_rng,
-        #     num_samples=cfg.data.n_train,
-        #     dataset="train",
-        # )
-        # rng, next_rng = jax.random.split(rng)
-        # dataloader = ndp.data.dataloader(
-        #     data,
-        #     batch_size=cfg.optim.batch_size,
-        #     key=next_rng,
-        #     n_points=cfg.data.n_points,
-        # )
-        # batch0 = next(dataloader)
-        # x, y = batch0.xs, batch0.ys
 
         x = jax.random.normal(next_rng, (cfg.optim.batch_size, cfg.data.n_points, 2))
         rng, next_rng = jax.random.split(rng)
@@ -70,7 +41,6 @@ with initialize(config_path="../experiments/steerable_gp/config", version_base="
         rng, next_rng = jax.random.split(rng)
 
         t = jax.random.uniform(next_rng, (y.shape[0],), minval=0, maxval=1)
-        print("x, y", x.shape, y.shape)
         return x, y, t
 
     @pytest.fixture(name="denoise_model")
@@ -86,9 +56,6 @@ with initialize(config_path="../experiments/steerable_gp/config", version_base="
         return jax.jit(
             lambda x_, y_, t_: stateful_forward.apply(params, state, x_, y_, t_)[0]
         )
-        # init, apply = hk.without_apply_rng(hk.transform(model))
-        # params = init(rng, x, y, t)
-        # return jax.jit(lambda x_, y_, t_: apply(params, x_, y_, t_))
 
     def test_denoise_model_permutation_equivariance(rng, inputs, denoise_model):
         x, y, t = inputs
